7c81b7bcea47aa4891e2a6febbc7f0253a59be61
josdem <joseluis.delacruz@gmail.com>
2021-10-11 15:24:14 -0400

    Adding server proxy filter

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const { createProxyMiddleware } = require("http-proxy-middleware")
 const express = require("express")
 const server = express()
 const LOCAL_HOST = "localhost"
-const API_SERVICE_URL = "https://webflux.josdem.io/categories/en/"
+const API_SERVICE_URL = "https://webflux.josdem.io/categories/en"
 const PORT = 3000
 
-server.get("/", (req, res) => {
+server.get("/en", (req, res) => {
   res.send("Hello World!")
 })
+
+server.use("", (req, res, next) => {
+  console.log(`request: ${req}`)
+  next()
+})
+
+server.use(
+  "/adobe_mock",
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/adobe_mock`]: "",
+    },
+  })
+)
 
 server.listen(PORT, LOCAL_HOST, () => {
   console.log(`Example app listening at ${LOCAL_HOST}:${PORT}`)
