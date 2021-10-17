cda46b3c78b9b9797f5e28bd72a065e176ad29d0
josdem <joseluis.delacruz@gmail.com>
2021-10-17 19:11:11 -0400

    Capturing request

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const express = require("express")
-const httpProxy = require('http-proxy');
 const server = express()
 const LOCAL_HOST = "localhost"
 const API_SERVICE_URL = "http://localhost:8080/"
@@ -10,22 +9,16 @@ server.get("/info", (req, res) => {
   res.send("This proxy server wich proxies request to a third party service")
 })
 
-const proxy = httpProxy.createServer({
-  target:'http://localhost:3000/users'
-});
-
 server.use(
   "/users",
   createProxyMiddleware({
     target: API_SERVICE_URL,
-    changeOrigin: false
+    changeOrigin: false,
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(req)
+    }
   })
 )
-
-proxy.on('proxyReq', function (proxyReq, req, res) {
-  console.log('RAW Response from the target', JSON.stringify(proxyReq.headers, true, 2));
-  console.log(req)
-})
 
 server.listen(PORT, LOCAL_HOST, () => {
   console.log(`Example app listening at ${LOCAL_HOST}:${PORT}`)
